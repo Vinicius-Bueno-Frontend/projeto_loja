466e060ad8a967e3a65cece3cb8c162af614bddc
ViniciusGenthix <viniciusbuenoti@gmail.com>
2020-07-30 21:57:08 -0300

    CRUD da pasta contato testado e finalizado. Iniciando a pasta endereco.

---
+++ domain/endereco.php
@@ -0,0 +1,53 @@
+<?php
+
+class Endereco{
+    public $idendereco;
+    public $tipo;
+    public $logradouro;
+    public $numero;
+    public $complemento;
+    public $bairro;
+    public $cep;
+
+    public function __construct($db){
+        $this->conexao = $db;
+    }
+
+    public function listar(){
+        $query = "select * from endereco";
+
+        $stmt = $this->conexao->prepare($query);
+
+        $stmt->execute();
+
+        return $stmt;
+    }
+
+    public function cadastro(){
+        $query = "insert into endereco set tipo=:t, logradouro=:l, numero=:n, 
+        complemento=:c, bairro=:b, cep=:cep";
+
+        $stmt = $this->conexao->prepare($query);
+
+        /*Vamos vincular os dados que veem do app ou navegador com os campos de
+        banco de dados
+        */
+        $stmt->bindParam(":t",$this->tipo);
+        $stmt->bindParam(":l",$this->logradouro);
+        $stmt->bindParam(":n",$this->numero);
+        $stmt->bindParam(":c",$this->complemento);
+        $stmt->bindParam(":b",$this->bairro);
+        $stmt->bindParam(":cep",$this->cep);
+
+        if($stmt->execute()){
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+
+}
+
+?>
---
+++ service/contato/alterartelefone.php
@@ -0,0 +1,54 @@
+<?php
+
+/*
+Vamos construir os cabeçalhos para trabalho com a api
+*/
+header("Access-Control-Allow-Origin:*");
+header("Content-Type:application/json;charset=utf-8");
+
+/*Para efetuar a cadastro de dados no banco é preciso
+informar a api que essa ação irá ocorrer com o método PUT, que
+é responsável pela atualização de dados da api.
+*/
+header("Access-Control-Allow-Methods:PUT");
+
+include_once "../../config/database.php";
+
+include_once "../../domain/contato.php";
+
+$database = new Database();
+$db = $database->getConnection();
+
+$contato = new Contato($db);
+
+/*
+O cliente irá enviar os dado no formato Json. Porém
+ nós precisamos dos dados no formato php para cadastrar em
+ banco de dados. 
+ Para realizar essa conversão iremos usar o comando json_decode
+ Assim o cliente envia os dados e estes serão convertidos para php
+*/
+$data = json_decode(file_get_contents("php://input"));
+
+#Verificar se os dados vindos do usuário estão preenchidos
+if(!empty($data->telefone) && !empty($data->idcontato)){
+
+    $contato->telefone = $data->telefone;
+    $contato->idcontato = $data->idcontato;
+
+
+    if($contato->alterarTelefone()){
+        header("HTTP/1.0 201");
+        echo json_encode(array("mensagem"=>"Telefone alterado com sucesso!"));
+    }
+    else{
+        header("HTTP/1.0 400");
+        echo json_encode(array("mensagem"=>"Não foi possível alterar o telefone."));
+    }
+}
+else{
+    header("HTTP/1.0 400");
+    echo json_encode(array("mensagem"=>"Você precisa preencher todos os campos"));
+}
+
+?>
