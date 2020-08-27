ea7d10a86815ee5602d1a7352920b3fcf1566f90
ViniciusGenthix <viniciusbuenoti@gmail.com>
2020-08-27 18:44:52 -0300

    Tela de Splash

--- domain/produto.php
+++ domain/produto.php
@@ -1,27 +1,58 @@
 <?php
 
 class Produto{
-    public $idproduto;
-    public $nomeproduto;
-    public $descricao;
-    public $preco;
-    public $idfoto;
 
-    public function __construct($db){
-        $this->conexao = $db;
+public $idproduto;
+public $nomeproduto;
+public $descricao;
+public $preco;
+public $idfoto;
+
+
+public function __construct($db){
+    $this->conexao = $db;
+}
+
+public function listar(){
+    $query = "select * from produto";
+
+    $stmt = $this->conexao->prepare($query);
+
+    $stmt->execute();
+
+    return $stmt;
+}
+
+public function listarTelaInicial(){
+    $query = "select p.idproduto,p.nomeproduto, p.preco, f.foto1 from produto p inner join foto f on p.idfoto=f.idfoto";
+    $stmt = $this->conexao->prepare($query);
+
+    $stmt->execute();
+
+    return $stmt;
+}
+
+public function cadastro(){
+    $query = "insert into produto set nomeproduto=:np, descricao=:d, preco=:p, idfoto=:if";
+
+    $stmt = $this->conexao->prepare($query);
+
+    /*Vamos vincular os dados que veem do app ou navegador com os campos de
+    banco de dados
+    */
+    $stmt->bindParam(":np",$this->nomeproduto);
+    $stmt->bindParam(":d",$this->descricao);
+    $stmt->bindParam(":p",$this->preco);
+    $stmt->bindParam(":if",$this->idfoto);
+
+    if($stmt->execute()){
+        return true;
+    }
+    else{
+        return false;
     }
 
-    public function listar(){
-        $query = "select * from produto";
-
-        $stmt = $this->conexao->prepare($query);
-
-        $stmt->execute();
-
-        return $stmt;
-    }
-
-
+}
 }
 
 ?>
